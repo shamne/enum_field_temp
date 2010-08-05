94cf7f3c2dc5b84cb5528070bfbf80984a612ac3
Alexander Shamne <alexander.shamne@gmail.com>
2010-08-05 10:18:12 -0700

    Initial commit to enum_field_temp.

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'enum_field_temp'
+
+class Test::Unit::TestCase
+end
